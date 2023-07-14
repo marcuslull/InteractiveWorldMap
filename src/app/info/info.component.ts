8b3099dd4629ff531f00f337018fbc803081fb6a
Marcus Lull <marcuslull@gmail.com>
2023-07-14 12:16:34 -0700

    implement httpClient, final styling, and comments

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,22 +1,33 @@
 import {Component, DoCheck, Input} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
+
 export class InfoComponent implements DoCheck{
+
+  // need an HttpClient instance for the API calls
+  constructor(private http: HttpClient) {
+  }
+
+  // check if the id input value has changed
+  // if is has lock in the id and begin the data retrieval process by calling getCountryInfo()
   ngDoCheck(): void {
-    if (this.lastID !== this.id) {
-      this.lastID = this.id;
+    if (this.lockedID !== this.id) {
       this.lockedID = this.id;
       this.getCountryInfo();
     }
   }
 
-  @Input() id: any;
-  lastID: any;
+  // TS variables
+  @Input() id: any; // input from map module via parent
   lockedID: any;
+  countries: Country[] = []; // stores retrieved api data in the form of Country objects
+
+  // HTML variables
   countryName: any;
   capitalCity: any;
   region: any;
@@ -24,8 +35,9 @@ export class InfoComponent implements DoCheck{
   longitude: any;
   latitude: any;
 
-  countries: Country[] = [];
-
+  // checks for a Country data locally before retrieving from the api
+  // if a local Country object is found, we set the HTML variables with the appropriate data and call displayInfo()
+  // if country data is not found locally we will make an api call
   getCountryInfo() {
     for (const country of this.countries) {
       if (country.id === this.lockedID) {
@@ -36,34 +48,37 @@ export class InfoComponent implements DoCheck{
         this.longitude = country.long;
         this.latitude = country.lat;
         this.displayInfo()
-        this.lockedID = "";
+        this.lockedID = ""; // if we don't clear the locked ID we could get an infinite loop in some cases
         return;
       }
     }
+    // need to make sure the api doesn't get called on an empty string
     if (this.lockedID.length !== 0) {
       this.apiCall();
     }
   }
 
+  // responsible for injecting the HTML to the page using the HTML variables
   displayInfo() {
     document.getElementById("div").innerHTML =
-      "<h2>" + this.countryName + "</h2>\n" +
+      "<h3>" + this.countryName + "</h3>\n" +
       "<ul>\n" +
-      "  <li>" + this.capitalCity + "</li>\n" +
-      "  <li>" + this.region + "</li>\n" +
-      "  <li>" + this.incomeLevel + "</li>\n" +
-      "  <li>" + this.longitude + "</li>\n" +
-      "  <li>" + this.latitude + "</li>\n" +
+      "  <li><strong>Capital city: </strong>" + this.capitalCity + "</li>\n" +
+      "  <li><strong>Region: </strong>" + this.region + "</li>\n" +
+      "  <li><strong>Income level: </strong>" + this.incomeLevel + "</li>\n" +
+      "  <li><strong>Longitude: </strong>" + this.longitude + "</li>\n" +
+      "  <li><strong>Latitude: </strong>" + this.latitude + "</li>\n" +
       "</ul>"
   }
 
-  async apiCall() {
+  // if the country data is not found locally we make an API call to WorldBank
+  // once we have the data we will instantiate a new Country object and push it to the countries variable
+  // lastly, call getCountryInfo to handle retrieval and display of the now local country object
+  apiCall() {
     let urlString = "https://api.worldbank.org/v2/country/"
     let countryIdentifier = `${this.lockedID}?format=json`;
-    const response = await fetch(urlString + countryIdentifier);
-    if (response.ok) {
-      let data = await response.json();
-      let goodPartOfTheData = data[1][0];
+    this.http.get(urlString + countryIdentifier).subscribe(data => {
+      let goodPartOfTheData = data[1][0]; // isolate the part of the json we are interested in
       let parsed = new Country()
       parsed.id = this.lockedID;
       parsed.name = goodPartOfTheData.name;
@@ -73,13 +88,12 @@ export class InfoComponent implements DoCheck{
       parsed.long = goodPartOfTheData.longitude;
       parsed.lat = goodPartOfTheData.latitude;
       this.countries.push(parsed);
-      this.getCountryInfo();
-    } else {
-      console.log(response.status)
-    }
+      this.getCountryInfo(); // this is a call back to the calling function, has the effect of continuous API requests until successful
+    })
   }
 }
 
+// a class model - only needed in this component so no need to export
 class Country {
   id: string;
   name: string;
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -5,16 +5,20 @@ import {Component, EventEmitter, Output} from '@angular/core';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent{
+
+  // need an emitter to move data to sibling component via parent component
   @Output() emitter = new EventEmitter<any>()
 
+  // called by a mouseover event in HTML. gets the id of the current element
+  // if the current element ID is not null we want to send that off to the info component for processing
   getValue() {
-      // @ts-ignore
       const currentElement = event.target;
       // @ts-ignore
       const id = currentElement.getAttribute("id")
     if (id != null) {
-      this.emitter.emit(id);
+      this.emitter.emit(id); // destination: info.component.ts
     }
   }
 }
